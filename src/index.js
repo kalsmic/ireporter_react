624245340b090e6de46a18443fd3a3969b1ebb1f
Arthur Kalule <arthur.kalule@andela.com>
2019-05-22 14:16:48 +0300

    set up react project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello IReporter!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
